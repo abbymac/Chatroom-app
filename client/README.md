babc449b15b17c888f69714d300601cbf5aafafc
abbymac <abby.mcinerney4433@gmail.com>
2020-08-04 13:45:58 -0400

    changed elliptic dependencies and readme

--- client/README.md
+++ client/README.md
@@ -3,7 +3,7 @@
 ### Usage
 
 1. Clone repo
-2. install dependencies
+2. install dependencies 
 3. run npm start in server folder
 4. run yarn start in client folder
 5. go to localhost:3000
@@ -12,3 +12,15 @@
 ### Design
 
 Two pages: login screen and chatroom screen. User logs in by specifying a username and chatroom name. If chatroom is already in use, then user enters existing chatroom. If chatroom is new, then user enters empty chatroom. Opening new tabs to localhost:3000 simulates multiple users. When a 'new' user enters an existing chatroom, the other occupants are alerted by the admin.
+
+#### Demo
+
+![demogif](https://media.giphy.com/media/j3tI4rXfhoUDChNcoS/giphy.gif)
+
+#### Chatroom Screen
+
+
+Now you can talk to yourself!
+
+
+![a](https://media.giphy.com/media/otM1VWfLLa4uI/giphy.gif)
--- server/index.js
+++ server/index.js
@@ -1,46 +1,63 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 
-const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js')
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
 
 const PORT = process.env.PORT || 5000;
 
-const router = require('./router');
+const router = require("./router");
 
 const app = express();
 
 const server = http.createServer(app);
+console.log("server is", server);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    socket.on('join', ({name, room}, callback) => {
-        const {error, user} = addUser({id: socket.id, name, room});
-        if(error) return callback(error);
+io.on("connection", (socket) => {
+  console.log("in connection now.");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
 
-        socket.emit('message', { user: 'admin', text: `${user.name} welcome to the room ${user.room}` })
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined.`})
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name} welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined.` });
 
-        socket.join(user.room);
+    socket.join(user.room);
 
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
-        callback();
-    })
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();
+  });
 
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-        io.to(user.room).emit('message', { user: user.name, text: message });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
-        callback();
-    })
+  socket.on("sendMessage", (message, callback) => {
+    debugger;
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();
+  });
 
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-        if(user){
-            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`})
-        }
-    })
-})
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left.`,
+      });
+    }
+  });
+});
 
 app.use(router);
 
